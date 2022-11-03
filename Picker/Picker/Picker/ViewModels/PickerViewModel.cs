9cbbbaefffacf5c45eb9337af60bea3b690829aa
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-11-03 17:07:03 +0700

    done picker

--- Picker/Picker/Picker/ViewModels/PickerViewModel.cs
+++ Picker/Picker/Picker/ViewModels/PickerViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using Picker.Models;
 using Picker.Views;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using XF.Material.Forms.UI;
@@ -14,6 +16,7 @@ namespace Picker.ViewModels
     public class PickerViewModel : BaseViewModel
     {
         public ObservableCollection<City> Cities { get; }
+        public ObservableCollection<Country> Countries { get; }
         public Command LoadCitiesCommand { get; }
         public Command LoadPickerCommand { get; }
         //public Command AddCityCommand { get; }
@@ -23,6 +26,8 @@ namespace Picker.ViewModels
             Title = "Picker";
 
             Cities = new ObservableCollection<City>();
+
+            Countries = new ObservableCollection<Country>();
 
             LoadCitiesCommand = new Command(async () => await ExecuteLoadCitiesCommand());
 
@@ -34,17 +39,43 @@ namespace Picker.ViewModels
 
         private async Task LoadPickerCommandAsync()
         {
-            var country = new string[]
-{
-    "Tất cả",
-    "Việt Nam",
-    "Đức",
-    "Bồ Đào Nha"
-};
-
+            List<string> countrylist = new List<string>
+            {
+                "Tất cả"
+            };
+            Countries.Clear();
+            var countries = await CountryDataStore.GetCountriesAsync(true);
+            foreach (var country in countries)
+            {
+                countrylist.Add(country.CountryName);
+            }
+            String[] str = countrylist.ToArray();
             //Show confirmation dialog for choosing one.
-            var result = await MaterialDialog.Instance.SelectChoiceAsync(title: "Chọn một đất nước",
-                                                                         choices: country);
+            var result = await MaterialDialog.Instance.SelectChoiceAsync(title: "Chọn một đất nước", choices: str);
+            if (result != -1)
+            {
+                            IsBusy = true;
+            try
+            {
+                Cities.Clear();
+                var cities = await CityDataStore.GetCitiesAsync(true);
+                foreach (var city in cities)
+                {
+                    if (result.ToString() != "0" && city.CountryID == result.ToString())
+                        Cities.Add(city);
+                    else if (result.ToString() == "0")
+                        Cities.Add(city);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+            }
         }
 
         async Task ExecuteLoadCitiesCommand()
