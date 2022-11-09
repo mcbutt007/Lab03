37908fd688118da8d38cef0a4a7a7dd81fd829f0
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-11-09 09:36:23 +0700

    checkpoint

---
+++ App Shell FlyoutPage/App Shell FlyoutPage/App Shell FlyoutPage/ViewModels/LaptopGamingViewModel.cs
@@ -0,0 +1,52 @@
+﻿using App_Shell_FlyoutPage.Models;
+using App_Shell_FlyoutPage.Views;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App_Shell_FlyoutPage.ViewModels
+{
+    public class LaptopGamingViewModel : BaseViewModel
+    {
+        public ObservableCollection<Laptop> Laptops { get; }
+        public Command LoadLaptopsCommand { get; }
+
+        public LaptopGamingViewModel()
+        {
+            Title = "Gaming Laptop";
+            Laptops = new ObservableCollection<Laptop>();
+            LoadLaptopsCommand = new Command(async () => await ExecuteLoadLaptopsCommand());
+
+        }
+
+        async Task ExecuteLoadLaptopsCommand()
+        {
+            IsBusy = true;
+
+            try
+            {
+                Laptops.Clear();
+                var items = await LaptopDataStore.GetLaptopsAsync(true);
+                foreach (var item in items)
+                {
+                    Laptops.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        public void OnAppearing()
+        {
+            IsBusy = true;
+        }
+    }
+}
---
+++ App Shell FlyoutPage/App Shell FlyoutPage/App Shell FlyoutPage/Views/LaptopGamingPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using App_Shell_FlyoutPage.Models;
+using App_Shell_FlyoutPage.ViewModels;
+using App_Shell_FlyoutPage.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Shell_FlyoutPage.Views
+{
+    public partial class LaptopGamingPage : ContentPage
+    {
+        LaptopGamingViewModel _viewModel;
+
+        public LaptopGamingPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new LaptopGamingViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
