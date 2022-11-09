d11e30d9451b25732e24e8998b5168584f8a2c44
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-11-09 19:03:08 +0700

    checkpoint

---
+++ App Shell FlyoutPage/App Shell FlyoutPage/App Shell FlyoutPage/Views/IphonePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using App_Shell_FlyoutPage.Models;
+using App_Shell_FlyoutPage.ViewModels;
+using App_Shell_FlyoutPage.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Shell_FlyoutPage.Views
+{
+    public partial class IphonePage : ContentPage
+    {
+        IphoneViewModel _viewModel;
+
+        public IphonePage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new IphoneViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
---
+++ App Shell FlyoutPage/App Shell FlyoutPage/App Shell FlyoutPage/Views/SamsungSmartphonePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using App_Shell_FlyoutPage.Models;
+using App_Shell_FlyoutPage.ViewModels;
+using App_Shell_FlyoutPage.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Shell_FlyoutPage.Views
+{
+    public partial class SamsungSmartphonePage : ContentPage
+    {
+        SamsungSmartphoneViewModel _viewModel;
+
+        public SamsungSmartphonePage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new SamsungSmartphoneViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
