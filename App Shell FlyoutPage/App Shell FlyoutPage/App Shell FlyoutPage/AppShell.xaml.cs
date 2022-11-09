3ae54c0909daa49a2df3ecd23c3d78b33047075f
Phuc Hoang <hoangmaithienphuc@gmail.com>
2022-11-10 00:24:33 +0700

    checkpoint

--- App Shell FlyoutPage/App Shell FlyoutPage/App Shell FlyoutPage/AppShell.xaml.cs
+++ App Shell FlyoutPage/App Shell FlyoutPage/App Shell FlyoutPage/AppShell.xaml.cs
@@ -11,13 +11,14 @@ namespace App_Shell_FlyoutPage
         public AppShell()
         {
             InitializeComponent();
+            SetTabBarIsVisible(this, false);
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
         {
-            await Shell.Current.GoToAsync("//LoginPage");
+            await Shell.Current.GoToAsync("//AboutPage");
         }
     }
 }
---
+++ App Shell FlyoutPage/App Shell FlyoutPage/App Shell FlyoutPage/Views/HomePage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using App_Shell_FlyoutPage.Models;
+using App_Shell_FlyoutPage.ViewModels;
+using App_Shell_FlyoutPage.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App_Shell_FlyoutPage.Views
+{
+    public partial class HomePage : ContentPage
+    {
+        HomeViewModel _viewModel;
+
+        public HomePage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new HomeViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
